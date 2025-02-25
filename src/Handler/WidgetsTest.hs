0a2a840f549c785b602ed3a0577c5d5a47d0212e
GabrielaAndocilla <gabrielaandocilla@hotmail.com>
2025-02-25 14:26:49 +0100

    set project

---
+++ src/Handler/WidgetsTest.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+module Handler.WidgetsTest where
+
+import Import
+import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
+import Text.Julius (RawJS (..))
+
+
+getWidgetsTestR :: Handler Html
+getWidgetsTestR = defaultLayout $ do
+    setTitle "Widget Page"
+    toWidget [lucius| h1 { color: green; } |]
+    addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"
+    toWidget
+        [julius|
+            $(function() {
+                $("h1").click(function(){
+                    alert("You clicked on the heading!");
+                });
+            });
+        |]
+    toWidgetHead
+        [hamlet|
+            <meta name=keywords content="some sample keywords">
+        |]
+    toWidget
+        [hamlet|
+            <h1>Here's one way of including content
+        |]
+    [whamlet|<h2>Here's another |]
+    toWidgetBody
+        [julius|
+            alert("This is included in the body itself");
+        |]
+
